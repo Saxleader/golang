f17f1466c451bff63d2e35f91bf6806273fb741f
Saxleader <coreydihel@gmail.com>
2015-03-16 21:41:28 -0700

    submitting 3/18 assignment

--- 3-18_assign/FormPage.go
+++ 3-18_assign/FormPage.go
@@ -1,9 +1,30 @@
-package main
+package FormPage
+
+import (
+	"html/template"
+	"net/http"
+)
 
 func init() {
-	HandleFunc("/", handler)
+	http.Handle("/hiddenDir/", http.StripPrefix("/hiddenDir/", http.FileServer(http.Dir("assets/"))))
+
+	http.HandleFunc("/", root)
+	http.HandleFunc("/response", response)
 }
 
-func handler() {
+func root(rw http.ResponseWriter, req *http.Request) {
+	mytemp := template.Must(template.ParseFiles("assets/layout.html", "assets/root.html"))
+	mytemp.ExecuteTemplate(rw, "layout", nil)
+}
 
+func response(rw http.ResponseWriter, req *http.Request) {
+	name := req.FormValue("name")
+	switch name {
+	case "Corey Dihel":
+		mytemp := template.Must(template.ParseFiles("assets/layout.html", "assets/master.html"))
+		mytemp.ExecuteTemplate(rw, "layout", nil)
+	default:
+		mytemp := template.Must(template.ParseFiles("assets/layout.html", "assets/guest.html"))
+		mytemp.ExecuteTemplate(rw, "layout", name)
+	}
 }
