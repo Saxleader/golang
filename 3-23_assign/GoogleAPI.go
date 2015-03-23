306e568e4e1c4e34e5fa79bf49d9d794fed1d7a3
Corey Dihel <coreydihel@gmail.com>
2015-03-23 14:44:54 -0700

    More for Google API gmail

--- 3-23_assign/GoogleAPI.go
+++ 3-23_assign/GoogleAPI.go
@@ -1,11 +1,19 @@
 package googleAPI
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
-	"fmt"
 )
+
+type resultinput struct {
+	Search string
+	Emails []string
+}
 
 func init() {
 	http.HandleFunc("/", apiQuery)
@@ -14,21 +22,54 @@ func init() {
 
 func apiQuery(rw http.ResponseWriter, req *http.Request) {
 	t := template.Must(template.ParseFiles("assets/apiQuery"))
-	err = t.Execute(rw, nil)
+	err := t.Execute(rw, nil)
 	if err != nil {
-		http.Error(rw, err.error(), http.StatusInternalServerError)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func results(rw http.ResponseWriter, req *http.Request) {
-	mysearch := req.FormValue("search")
+	var mysearch = resultinput{Search: req.FormValue("search")}
 
-	safeAddr := url.QueryEscape(mysearch)
-	fullURL := fmt.Sprintf("https://www.googleapis.com/gmail/v1/users/me/messages?q=", ...)
+	safeAddr := url.QueryEscape(mysearch.Search)
+	fullURL := fmt.Sprintf("https://www.googleapis.com/gmail/v1/users/me/messages?q=%s", mysearch)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+	}
+
+	resp, requestErr := client.Do(req)
+	if requestErr != nil {
+		log.Fatal("Do: ", requestErr)
+	}
+
+	defer resp.Body.Close()
+
+	body, dataReadErr := ioutil.ReadAll(resp.Body)
+	if dataReadErr != nil {
+		log.Fatal("ReadAll: ", dataReadErr)
+	}
+
+	res := make(map[string][]map[string]interface{}, 0)
+
+	json.Unmarshal(body, &res)
+
+	for i, _ := range res["messages"] {
+		append(mysearch.Emails, res["messages"][i]["id"])
+	}
+
+	var myemails []string
+
+	for i, value := range mysearch.Emails {
+		mysearch.Emails[i] = "https://mail.google.com/mail/u/0/#inbox/" + value
+	}
 
 	t := template.Must(template.ParseFiles("assets/results.html"))
-	err = t.Execute(rw, data)
+	err = t.Execute(rw, mysearch)
 	if err != nil {
-		http.Error(rw, err.error(), http.StatusInternalServerError)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
 }
