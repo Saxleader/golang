f0651243ecc101381725d133e2cc03fb6ab78209
Corey Dihel <coreydihel@gmail.com>
2015-04-15 13:49:46 -0700

    finishing up some things

--- 3-16_assign/StaticPage/website.go
+++ 3-16_assign/StaticPage/website.go
@@ -1,13 +1,20 @@
-package main
+package staticpage
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func init() {
+	http.Handle("/hiddenDir/", http.StripPrefix("/hiddenDir/", http.FileServer(http.Dir("assets/"))))
 	http.HandleFunc("/", handler)
 }
 
 func handler(rw http.ResponseWriter, req *http.Request) {
-
+	t := template.Must(template.ParseFiles("index.html"))
+	err := t.ExecuteTemplate(rw, "index.html", nil)
+	if err != nil {
+		log.Fatalf("Error: %v", err.Error())
+	}
 }
