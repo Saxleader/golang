cc48202dcd41361ff75fdd723e231246c8ecb4b7
Corey Dihel <coreydihel@gmail.com>
2015-04-29 14:49:02 -0700

    finished session assignment

--- 4-20_assign/mysession.go
+++ 4-20_assign/mysession.go
@@ -1,136 +1,55 @@
-package main
+package mysessions
 
 import (
-	"container/list"
-	"crypto/rand"
-	"encoding/base64"
-	"fmt"
-	"io"
+	"html/template"
 	"net/http"
-	"net/url"
-	"sync"
-	"time"
+
+	"github.com/astaxie/beego/session"
 )
 
-type Manager struct {
-	cookieName  string
-	lock        sync.Mutex
-	provider    Provider
-	maxlifetime int64
+var globalSessions *session.Manager
+
+var mytmpl *template.Template
+
+type userdata struct {
+	User    string
+	Message string
 }
-
-func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, error) {
-	provider, ok := provides[provideName]
-	if !ok {
-		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
-	}
-	return &Manager{provider: provider, cookieName: cookieName, maxlifetime: maxlifetime}, nil
-}
-
-func (manager *Manager) sessionID() string {
-	b := make([]byte, 32)
-	if _, err := io.ReadFull(rand.Reader, b); err != nil {
-		return ""
-	}
-	return base64.URLEncoding.EncodeToString(b)
-}
-
-func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
-	manager.lock.Lock()
-	defer manager.lock.Unlock()
-	cookie, err := r.Cookie(manager.cookieName)
-	if err != nil || cookie.Value == "" {
-		sid := manager.sessionID()
-		session, _ = manager.provider.SessionInit(sid)
-		cookie := &http.Cookie{Name: manager.cookieName, Value: url.QueryEscape(sid), Path: "/", HttpOnly: true, MaxAge: int(manager.maxlifetime)}
-		http.SetCookie(w, cookie)
-	} else {
-		sid := url.QueryEscape(cookie.Value)
-		session, _ = manager.provider.SessionRead(sid)
-	}
-	return
-}
-
-func (manager *Manager) SessionDestroy(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie(manager.cookieName)
-	if err != nil || cookie.Value == "" {
-		return
-	} else {
-		manager.lock.Lock()
-		defer manager.lock.Unlock()
-		manager.provider.SessionDestroy(cookie.Value)
-		expiration := time.Now()
-		cookie := http.Cookie{Name: manager.cookieName, Path: "/", HttpOnly: true, Expires: expiration, MaxAge: -1}
-		http.SetCookie(w, &cookie)
-	}
-}
-
-func (manager *Manager) GC() {
-	manager.lock.Lock()
-	defer manager.lock.Unlock()
-	manager.provider.SessionGC(manager.maxlifetime)
-	time.AfterFunc(time.Duration(manager.maxlifetime), func() { manager.GC() })
-}
-
-// type Provider interface {
-// 	SessionInit(sid string) (Session, error)
-// 	SessionRead(sid string) (Session, error)
-// 	SessionDestroy(sid string) error
-// 	SessionGC(maxLifeTime int64)
-// }
-
-type Provider struct {
-	lock     sync.Mutex
-	sessions map[string]*list.Element
-	list     *list.List
-}
-
-func (pder *Provider) SessionInit(sid string) (Session, error) {
-	pder.lock.Lock()
-	defer pder.lock.Unlock()
-	v := make(map[interface{}]interface{}, 0)
-	newsess := &SessionStore{sid: sid, timeAccessed: time.Now(), value: v}
-	element := pder.list.PushBack(newsess)
-	pder.sessions[sid] = element
-	return newsess, nil
-}
-
-var pder = &Provider{lsit: list.New()}
-
-type Session interface {
-	Set(key, value interface{}) error
-	Get(key interface{}) interface{}
-	Delete(key interface{}) error
-	SessionID() string
-}
-
-type SessionStore struct {
-	sid          string
-	timeAccessed time.Time
-	value        map[interface{}]interface{}
-}
-
-func (st *SessionStore) Set(key, value interface{}) error {
-	st.value[key] = value
-	pder.SessionUpdate(st.sid)
-	return nil
-}
-
-var provides = make(map[string]Provider)
-
-func Register(name string, provider Provider) {
-	if provider == nil {
-		panic("session: Register provide is nil")
-	}
-	if _, dup := provides[name]; dup {
-		panic("session: Register called twice for provide " + name)
-	}
-	provides[name] = provider
-}
-
-var globalSessions *Manager
 
 func init() {
-	globalSessions, err := NewManager(provideName, cookieName, maxlifetime)
+	globalSessions, _ = session.NewManager("memory", `{"cookieName":"gosessionid", "enableSetCookie,omitempty": true, "gclifetime":3600, "maxLifetime": 3600, "secure": false, "sessionIDHashFunc": "sha1", "sessionIDHashKey": "", "cookieLifeTime": 3600, "providerConfig": ""}`)
 	go globalSessions.GC()
+	http.Handle("/hiddenDir/", http.StripPrefix("/hiddenDir/", http.FileServer(http.Dir("assets/"))))
+	http.HandleFunc("/", myroot)
+	http.HandleFunc("/store", store)
+	http.HandleFunc("/erase", erase)
+	mytmpl = template.Must(template.ParseFiles("assets/myroot.html"))
+}
+
+func myroot(rw http.ResponseWriter, req *http.Request) {
+	sess, _ := globalSessions.SessionStart(rw, req)
+	var mydata userdata
+	tempdata := sess.Get("userdata")
+	if tempdata != nil {
+		mydata = tempdata.(userdata)
+	}
+	mytmpl.ExecuteTemplate(rw, "myroot.html", mydata)
+}
+
+func store(rw http.ResponseWriter, req *http.Request) {
+	sess, _ := globalSessions.SessionStart(rw, req)
+	var mydata userdata
+	mydata.User = req.FormValue("name")
+	mydata.Message = req.FormValue("memo")
+	sess.Set("userdata", mydata)
+	http.Redirect(rw, req, "/", http.StatusFound)
+}
+
+func erase(rw http.ResponseWriter, req *http.Request) {
+	sess, _ := globalSessions.SessionStart(rw, req)
+	userdata := sess.Get("userdata")
+	if userdata != nil {
+		globalSessions.SessionDestroy(rw, req)
+	}
+	http.Redirect(rw, req, "/", http.StatusFound)
 }
